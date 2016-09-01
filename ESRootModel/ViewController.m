cf1973c8c9383521acb4bf3d20f5b62e6a96fa23
eShow <steamxys2013@163.com>
2016-09-01 14:41:43 +0800

    修改介绍

--- ESRootModel/ViewController.m
+++ ESRootModel/ViewController.m
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel * title = [[UILabel alloc]init];
+    [title setBounds:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 30)];
+    [title setCenter:CGPointMake(title.bounds.size.width/2, 60)];
+    [title setFont:[UIFont boldSystemFontOfSize:30]];
+    [title setText:@"ESRootModel"];
+    [title setTextAlignment:NSTextAlignmentCenter];
+    [self.view addSubview:title];
+    
+    UILabel * desLabel = [[UILabel alloc]init];
+    [desLabel setText:@"  请在控制台看LOG\n  test方法:创建一个TestModel类，通过setter为model对象赋值，然后创建另外一个TestModel类，用之前创建的model类生成的字典为新的model对象赋值\n\n\n   注意:本类目前版本不能识别非对象的属性,所有的属性必须使用OC类,int、float、double、long等基础类型请使用NSNumber"];
+    [desLabel setFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width - 20, 20)];
+    [desLabel setLineBreakMode:NSLineBreakByCharWrapping];
+    [desLabel setNumberOfLines:0];
+    [desLabel sizeToFit];
+    [desLabel setCenter:CGPointMake([UIScreen mainScreen].bounds.size.width/2, CGRectGetMaxY(title.frame) + desLabel.frame.size.height/2 + 20)];
+    [self.view addSubview:desLabel];
+    
+    [self test];
+}
+
+- (void)test{
     TestModel * model = [[TestModel alloc]init];
     [model setTestName:@"测试Name"];
     [model setTestId:@(10)];
@@ -32,6 +54,7 @@
         [model setSubId:@(i)];
         [array addObject:model];
     }
+    
     [subModel setSubModels:array];
     
     [model setSubModel:subModel];
